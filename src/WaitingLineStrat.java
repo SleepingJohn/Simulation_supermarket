5f56ee51308aa54a4d037533ce3003186d894295
Johannes Spilka <johannes.spilka@gmail.com>
2020-05-27 17:24:28 +0200

    insane strat

--- src/WaitingLineStrat.java
+++ src/WaitingLineStrat.java
@@ -11,12 +11,28 @@ public class WaitingLineStrat {
      * Ziel der Simulation: wie sieht eine möglichst "optimale" Wahl der Parameter w und m aus,
      * sodass die Kunden nicht zu lange warten müssen, aber auch die Kosten für besetzte Kassen nicht zu hoch werden.
       */
-    int n;      //Anzahl der Klassen
-    int w;      //Anzahl der Kunden
-    int max_w;  //Max Anzahl an Kunden die bei einer Kassa warten bis eine neue geöffnet wird (? glaub überflüssig)
-    int m;      //Anzahl Minuten, wo kein Kunde bedient wurde
+    /*
+    int n;      //Anzahl der Kassen
+    int w;      //Max Anzahl an Kunden die bei einer Kassa warten bis eine neue geöffnet wird.
+    double m;   //Anzahl Minuten, wo kein Kunde bedient wurde
+    double k;   //Kosten pro Kassa pro Minute
+    int t;      //Simulationszeit in min */
+public double strat(int n, int w, double m, double k, int t) {
+    int countperCustomer=2; //Es dauert 2min einen Kunden zu bedienen
+    int counter=0;
+    for(int i =0; i<t; i++){//iterates one min
+        // if()
+
+        if(counter==countperCustomer){
+            w--;
 
 
+        }
+        counter++;
+
+    }
+
+}
 
 }
 
